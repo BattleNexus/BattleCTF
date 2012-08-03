c4f4bd0abab8d1903642bde4b500ab6e7d43edd7
hypereddie10 <eddie@gamezgalaxy.com>
2012-08-03 03:56:44 -0400

    Started CTF

--- src/com/gamezgalaxy/ctf/main/main.java
+++ src/com/gamezgalaxy/ctf/main/main.java
@@ -1,12 +1,68 @@
 package com.gamezgalaxy.ctf.main;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Random;
+
 import com.gamezgalaxy.GGS.API.plugin.Game;
+import com.gamezgalaxy.GGS.chat.Messages;
 import com.gamezgalaxy.GGS.server.Server;
+import com.gamezgalaxy.ctf.gamemode.Gamemode;
+import com.gamezgalaxy.ctf.map.Map;
 
 public class main extends Game {
 
+	Messages globalchat;
+	Gamemode gm;
+	Thread run;
+	boolean running;
+	ArrayList<String> maps = new ArrayList<String>();
+	public static main INSTANCE;
+	public static final Random random = new Random();
 	public main(Server server) {
 		super(server);
+		globalchat = new Messages(server);
+	}
+
+	@Override
+	public void onLoad(String[] arg0) {
+		INSTANCE = this;
+		try {
+			loadMaps();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		run = new Game();
+		running = true;
+		run.start();
+	}
+	
+	public void loadMaps() throws IOException {
+		FileInputStream fstream = new FileInputStream("config/maps.config");
+		DataInputStream in = new DataInputStream(fstream);
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		String strLine;
+		while ((strLine = br.readLine()) != null) {
+			maps.add(strLine);
+		}
+		in.close();
+	}
+
+	@Override
+	public void onUnload() {
+		running = false;
+		try {
+			run.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -19,14 +75,37 @@ public class main extends Game {
 
 	@Override
 	public void Tick() {
+		if (gm.isRunning())
+			gm.tick();
 	}
-
-	@Override
-	public void onLoad(String[] arg0) {
+	
+	public static void GlobalMessage(String message) {
+		INSTANCE.globalchat.serverBroadcast(message);
 	}
-
-	@Override
-	public void onUnload() {
+	public static String secondsToTime(int sec) {
+		int min = 0;
+		while (sec >= 60) { sec -= 60; min++; }
+		return min + ":" + (sec < 10 ? "0" + sec : sec);
+	}
+	public class Game extends Thread {
+		
+		@Override
+		public void run() {
+			Map m = new Map();
+			while (running) {
+				String map = maps.get(random.nextInt(maps.size()));
+				m.mapname = map;
+				m.load();
+				gm = m.games.get(random.nextInt(m.games.size()));
+				gm.setup(m);
+				gm.roundStart();
+				try {
+					gm.waitForEnd();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 
 }
