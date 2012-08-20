c6a7e2cb98be0302155026fa5823f2c0de1f02cb
hypereddie10 <eddie@gamezgalaxy.com>
2012-08-20 14:45:20 -0400

    New files

---
+++ src/com/gamezgalaxy/ctf/gamemode/ctf/stalemate/actions/DropFlags.java
@@ -0,0 +1,24 @@
+package com.gamezgalaxy.ctf.gamemode.ctf.stalemate.actions;
+
+import com.gamezgalaxy.GGS.chat.ChatColor;
+import com.gamezgalaxy.ctf.gamemode.ctf.CTF;
+import com.gamezgalaxy.ctf.gamemode.ctf.stalemate.Action;
+import com.gamezgalaxy.ctf.gamemode.ctf.utl.Team;
+
+public class DropFlags extends Action {
+
+	@Override
+	public String getMessage() {
+		return ChatColor.Dark_Blue + "All Flags will be dropped!";
+	}
+
+	@Override
+	public void performaction(CTF c) {
+		for (int i = 0; i < c.holders.size(); i++) {
+			Team b = c.holders.get(c.holders.keySet().toArray()[i]);
+			c.resetFlag(b);
+			c.holders.remove(c.holders.keySet().toArray()[i]);
+		}
+	}
+
+}
